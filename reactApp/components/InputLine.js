09b2131a19e6ac2b42491fb4d2b20fbeb23384d0
danhuiZ <danhuizzz@gmail.com>
2017-06-30 15:46:53 -0700

    Another commit

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -4,12 +4,27 @@ import ReactDOM from 'react-dom';
 class InputLine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      typedText: ""
+    }
   }
+  handleTyping(event) {
+    this.setState({
+      typedText: event.target.value
+    })
+  }
+  handleSubmit(){
+    this.props.submit(this.state.typedText);
+    this.setState({
+      typedText: ""
+    })
+  }
+
   render() {
     return(
       <div>
-        <input type="text" placeholder="task"></input>
-        <button onClick={() => this.props.submit("Test task")} type="submit">Add todo</button>
+        <input type="text" placeholder="task" onChange={(event) => this.handleTyping(event)} value={this.state.typedText}></input>
+        <button onClick={() => this.handleSubmit()} type="submit">Add todo</button>
       </div>
     )
   }
