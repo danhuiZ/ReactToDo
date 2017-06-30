f8ddf9f04f32eda7f5a5b0a8086eb3df3f8e9b86
danhuiZ <danhuizzz@gmail.com>
2017-06-30 14:46:16 -0700

    Another commit

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,6 +3,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const dummyData =["Eat", "Sleep", "Beat Doudou", "Karaoke", "Live life"]
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+      <ul>
+        {this.props.todo.map((item) => <Todo key={item} item={item} />)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <li><input type="checkbox"></input>{this.props.item}</li>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <input type="text" placeholder="task"></input>
+        <button type="submit">Add todo</button>
+      </div>
+    )
+  }
+}
 
 
-ReactDOM.render(<p>Hello lives forever!</p>, document.getElementById('root'));
+// function TodoApp()
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <InputLine />
+        <TodoList todo={this.props.todo}/>
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(<TodoApp todo={dummyData} />, document.getElementById('root'));
